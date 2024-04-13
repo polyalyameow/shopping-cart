c0b2c2738f42c162f0fc63738290517a4f4ad3db
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-04-13 20:55:46 +0200

    cart in progress

---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,7 @@
+import Button from '@mui/material/Button';
+
+//types
+import { CartItemType } from '../App';
+import { Wrapper } from './CartItem.styles';
+
+const CartItem: React.FC = () => <div>Cart Item</div>;
