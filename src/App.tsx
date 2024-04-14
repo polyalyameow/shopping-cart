51125a87a4606bd9cab3bea1d3689850d40496e4
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-04-14 15:42:24 +0200

    increase amount in the cart

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Badge from '@mui/material/Badge';
 import { Wrapper, StyledButton } from './App.styles';
 
 import Item from './Item/Item';
+import Cart from './Cart/Cart';
 import { useState } from 'react';
 
 // Types
@@ -39,9 +40,20 @@ function App() {
     return items.reduce((ack: number, item) => ack + item.amount, 0)
   };
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    return setCartItems(prev => {
+      // is item already in cart?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id);
+      if (isItemInCart) {
+        return prev.map((item) => (
+          item.id === clickedItem.id ? {...item, amount: item.amount + 1} : item
+        ))}
+      // first time item is added
+      return [...prev, {...clickedItem, amount: 1}]
+    })
+  };
 
-  // const removeFromCart = () => null;
+  const handleRemoveFromCart = () => null;
 
   if (isLoading) return  <div>Loading...</div>;
   if (error) return <div>Something went wrong.</div>
@@ -51,7 +63,7 @@ function App() {
     <>
       <Wrapper>
         <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-          Cart goes here
+          <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart}/>
         </Drawer>
         <StyledButton onClick={() => setCartOpen(true)}>
           <Badge badgeContent={getTotalItems(cartItems)} color="error">
--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -7,3 +7,16 @@ type Props = {
     addToCart: (clickedItem: CartItemType) => void;
     removeFromCart: (id: number) => void;
 }
+
+const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => 
+{return (
+    <Wrapper>
+        <h2>Your Shopping Cart</h2>
+        {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+        {cartItems.map((item) => (
+            <CartItem key ={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart}/>
+        ))}
+    </Wrapper>
+)}
+
+export default Cart;
