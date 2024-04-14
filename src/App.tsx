37786da5df61ccd8620e5e57687e4fd1a4b7fc5d
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-04-14 15:54:13 +0200

    increment and total added

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,16 @@ function App() {
     })
   };
 
-  const handleRemoveFromCart = () => null;
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => (prev.reduce((ack, item) => {
+      if (item.id === id) {
+        if (item.amount === 1) return ack;
+        return [...ack, {...item, amount: item.amount - 1}]
+      } else {
+        return [...ack, item]
+      }
+    }, [] as CartItemType[])))
+  };
 
   if (isLoading) return  <div>Loading...</div>;
   if (error) return <div>Something went wrong.</div>
--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -8,14 +8,17 @@ type Props = {
     removeFromCart: (id: number) => void;
 }
 
-const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => 
-{return (
+const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {  
+    const calculateTotal = (items: CartItemType[]) => 
+    {return items.reduce((ack: number, item) => ack + item.amount * item.price, 0)}
+return (
     <Wrapper>
         <h2>Your Shopping Cart</h2>
         {cartItems.length === 0 ? <p>No items in cart.</p> : null}
         {cartItems.map((item) => (
             <CartItem key ={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart}/>
         ))}
+        <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
 )}
 
